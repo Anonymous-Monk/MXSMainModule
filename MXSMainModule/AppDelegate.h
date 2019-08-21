731bed3f6f7e5a19f96a4bbc5d1cfe7241291913
zerozhou <15666004981@163.com>
2019-08-21 09:11:55 +0800

    初始化项目

---
+++ MXSMainModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXSMainModule
+//
+//  Created by aicai on 2019/8/21.
+//  Copyright © 2019 mxsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXSMainModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXSMainModule
+//
+//  Created by aicai on 2019/8/21.
+//  Copyright © 2019 mxsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
